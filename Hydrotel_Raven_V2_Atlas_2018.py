75fb49b608957735775dc08807ba6cc4fccddd7f
Bijan55699 <bizhani1986@gmail.com>
2023-02-08 16:26:49 -0500

    final_version

--- Hydrotel_Raven_V2_Atlas_2018.py
+++ Hydrotel_Raven_V2_Atlas_2018.py
@@ -1,6 +1,24 @@
 """
 This script creates the subbasin map from a Hydrotel project directory
 
+Inputs:
+
+1- region_name = should be among following list:
+
+1- troncon_path: The path to The INFO_TRONCON.mat (provided by DEH) file which contains the river reaches (troncon) properties of the region.
+2- hydroteldir: The hydrotel project directory path
+3- workspace: The working directory to create the outputs
+
+Ouput:
+
+1- Subbasin polygon
+
+
+References:
+
+1- The river depth information is based on Hydrotel's depth-area relationship given in : https://onlinelibrary.wiley.com/doi/10.1002/hyp.10534
+2- The HydroLakes database is used for determining lake volume, depth and areas. reference: https://www.hydrosheds.org/products/hydrolakes
+3- The volum-area-depth relationship of lakes that are not identified by Hydrolakes is based on fitted relationship for Quebec's lake in :  https://agupubs.onlinelibrary.wiley.com/doi/full/10.1002/2016GL071378
 
 
 """
@@ -14,19 +32,23 @@ from rasterstats import zonal_stats
 import numpy as np
 
 
-def uhrh_to_sub(region, Troncon_path, workspace, Hydroteldir):
-    data = sio.loadmat(Troncon_path, struct_as_record=False, squeeze_me=True)
-    region_name = data[region]
-    size = region_name.shape[0]
-    shutil.copytree(Hydroteldir, workspace)
+def uhrh_to_sub(region_name, troncon_path, workspace, hydroteldir):
+    regions_list = ['ABIT_TRONCON','CNDA_TRONCON','CNDB_TRONCON','CNDC_TRONCON','CNDD_TRONCON','CNDE_TRONCON','GASP_TRONCON','LABI_TRONCON','MONT_TRONCON',
+                    'OUTM_TRONCON','OUTV_TRONCON','SAGU_TRONCON','SLNO_TRONCON','SLSO_TRONCON','VAUD_TRONCON']
+    if region_name not in regions_list:
+        print('the'+region_name+'is not recognized! please make sure the name is among the regions list!')
+    data = sio.loadmat(troncon_path, struct_as_record=False, squeeze_me=True)
+    region = data[region_name]
+    size = region.shape[0]
+    shutil.copytree(hydroteldir, workspace)
     os.chdir(workspace)
     df = []
     for i in range(size):
-        rec = region_name[i]
+        rec = region[i]
         df.append(
-            [rec.NOEUD_AVAL.NUMERO, rec.NOEUD_AMONT.NUMERO, rec.NO_TRONCON, rec.TYPE_NO, rec.LONGUEUR, rec.LARGEUR,
-             rec.UHRH_ASSOCIES, rec.C_MANNING, rec.PENTE_MOYENNE, rec.SUPERFICIE_DRAINEE])
-    Troncon_info = pd.DataFrame(df, columns={"NODE_AVAL": int,
+            [rec.NOEUD_AVAL.NUMERO, rec.NOEUD_AMONT.NUMERO, rec.NO_TRONCON, rec.TYPE_NO, rec.LONGUEUR, rec.LARGEUR, 0,
+             rec.UHRH_ASSOCIES, rec.C_MANNING, rec.PENTE_MOYENNE, rec.SUPERFICIE_DRAINEE, 0, 0, 0])
+    troncon_info = pd.DataFrame(df, columns={"NODE_AVAL": int,
                                              "NODE_AMONT": int,
                                              "SubId": int,
                                              "TYPE_NO": int,
@@ -39,28 +61,24 @@ def uhrh_to_sub(region, Troncon_path, workspace, Hydroteldir):
                                              "SA_Up": float,
                                              "Has_Gauge":int,
                                              "Lake_Cat":int,
-                                             "DowSubId":int,
-                                             },
+                                             "DowSubId":int},
                                 index=None,
                                 )
     # special values for non-river (lake) features
-    Troncon_info['Has_Gauge'] = 0  # Has_Gauge should be always 0 in the shapefile (strange?)
-    Troncon_info['BkfDepth'] = 0.13 * (Troncon_info['SA_Up'] ** 0.4)  # taken from equation 10 of Fossey et. al., 2015
-    Troncon_info.loc[Troncon_info.TYPE_NO == 2, 'Ch_n'] = 0.
-    Troncon_info.loc[Troncon_info.TYPE_NO == 2, 'BnkfWidth'] = 0.
-    Troncon_info.loc[Troncon_info.TYPE_NO == 2, 'RivLength'] = 0.
-    Troncon_info.loc[Troncon_info.TYPE_NO == 2, 'RivSlope'] = 0.
-    Troncon_info.loc[Troncon_info.TYPE_NO == 2, 'BkfDepth'] = 0.
-    Troncon_info['Lake_Cat'] = 0
-    Troncon_info.loc[Troncon_info.TYPE_NO == 2, 'Lake_Cat'] = 1 # for Lakes
-
+    troncon_info['Has_Gauge'] = 0  # Has_Gauge should be always 0 in the shapefile (strange?)
+    troncon_info['BkfDepth'] = 0.13 * (troncon_info['SA_Up'] ** 0.4)  # taken from equation 10 of Fossey et. al., 2015
+    troncon_info.loc[troncon_info.TYPE_NO == 2, {'Ch_n','BnkfWidth','RivLength','RivSlope','BkfDepth'}] = 0.
+    troncon_info['Lake_Cat'] = 0
+    troncon_info.loc[troncon_info.TYPE_NO == 2, 'Lake_Cat'] = 1 # for Lakes
+    # Assigning the SubId to uhrh geodataframe
     uhrh_fpth = os.path.join(workspace, "uhrh" + "." + "shp")  # The uhrh shape file created by Physitel
     uhrh = gpd.read_file(uhrh_fpth)
     uhrh['SubId'] = 0
 
+    # In this loop the SubIds will be added to the uhrh geodataframe.
     for i in range(size):
-        a = Troncon_info['ASSOCI_UHRH'][i]
-        id = Troncon_info['SubId'][i]
+        a = troncon_info['ASSOCI_UHRH'][i]
+        id = troncon_info['SubId'][i]
         print('writing subbasin :', i)
         if type(a) is int:
             aa = [a]
@@ -78,25 +96,26 @@ def uhrh_to_sub(region, Troncon_path, workspace, Hydroteldir):
                 if uhrh.loc[index, 'ident'] in dict.values():
                     uhrh.loc[index, 'SubId'] = id
 
+    # Dissolve the uhrh geodataframe based on SubIds
     uhrh_dissolve = uhrh.dissolve(by='SubId')
     uhrh_dissolve.reset_index(inplace=True)
     uhrh_dissolve['BasArea'] = uhrh_dissolve.area  # calculating the Area (m2) of each subbasin
-    # step3: finding the downstream subwatershed ID associated with each uhrh
 
-    Troncon_info['DowSubId'] = -1
+    # Finding the downstream subwatershed ID associated with each uhrh
+    troncon_info['DowSubId'] = -1
     for i in range(size):
-        naval = Troncon_info['NODE_AVAL'][i]
+        naval = troncon_info['NODE_AVAL'][i]
         for j in range(size):
-            namont = Troncon_info['NODE_AMONT'][j]
-            id = Troncon_info['SubId'][j]
+            namont = troncon_info['NODE_AMONT'][j]
+            id = troncon_info['SubId'][j]
             if type(namont) is int:
                 nal = [namont]
             else:
                 nal = namont.tolist()
             if naval in nal:  # if naval (downstream node) for reach i is upstream node for reach j, then reach j is downstream reach i
-                Troncon_info.loc[i, 'DowSubId'] = id
-    return Troncon_info, uhrh_dissolve
-def process_lakes(workspace, hydrolakes_file,Troncon_info,uhrh_dissolve):
+                troncon_info.loc[i, 'DowSubId'] = id
+    return troncon_info, uhrh_dissolve
+def process_lakes(workspace, hydrolakes_file,troncon_info,uhrh_dissolve):
     HyLAKES_Canada = gpd.read_file(hydrolakes_file)
     pth3 = os.path.join(workspace, "lacs" + "." + "shp")  # The lake shape file created by Physitel
     Hydrotel_lakes = gpd.read_file(pth3)
@@ -136,7 +155,6 @@ def process_lakes(workspace, hydrolakes_file,Troncon_info,uhrh_dissolve):
     lake_final['Hylak_id'].fillna(100, inplace=True)
 
     # Regional relationship for lake's volume-area for Quebec: ref: Heathcote et al., 2016.
-    test_lake = lake_final
     lake_final['Vol_total'].fillna(
         1 / 1000000 * (np.power(10, 1.165 * np.log10(lake_final['Shape_Area']) + np.log10(0.758))),
         inplace=True)  # volume is in MCM and area is in m
@@ -156,8 +174,8 @@ def process_lakes(workspace, hydrolakes_file,Troncon_info,uhrh_dissolve):
     lake_sub['LakeVol'] = lake_sub['Vol_total'] / 1000.  # To convert the volume in MCM in HydroLAKES database to km3
     lake_sub['LakeDepth'] = lake_sub['Depth_avg']
 
-def create_subbasin(Troncon_info,lake_sub):
-    subbasin = pd.merge(Troncon_info, lake_sub, on='SubId')
+def create_subbasin(troncon_info,lake_sub):
+    subbasin = pd.merge(troncon_info, lake_sub, on='SubId')
     # Lake data from HydroLAKES database
     subbasin['HyLakeId'] = subbasin['Hylak_id']
     subbasin['Laketype'] = subbasin['Lake_type']
@@ -242,16 +260,16 @@ def add_sub_attributes(workspace, subbasin):
     subbasin = subbasin.drop(['mean'], axis=1)
 
     # writing the final subbasin map
-    schema = gpd.io.file.infer_schema(subbasin)
-    schema['properties']['BnkfWidth'] = 'float'
-    schema['properties']['Ch_n'] = 'float'
-    schema['properties']['FloodP_n'] = 'float'
-    schema['properties']['HyLakeId'] = 'int'
-    schema['properties']['Laketype'] = 'int'
+    # schema = gpd.io.file.infer_schema(subbasin)
+    # schema['properties']['BnkfWidth'] = 'float'
+    # schema['properties']['Ch_n'] = 'float'
+    # schema['properties']['FloodP_n'] = 'float'
+    # schema['properties']['HyLakeId'] = 'int'
+    # schema['properties']['Laketype'] = 'int'
 
     os.chdir(workspace)
-    subbasin.to_file('subbasin.shp', schema=schema)
-
+    # subbasin.to_file('subbasin.shp', schema=schema)
+    subbasin.to_file('subbasin.shp')
 
 # def main():
 
@@ -259,9 +277,10 @@ def add_sub_attributes(workspace, subbasin):
 
 
 if __name__ == "__main__":
-    region = 'EST_TRONCON'
-    Troncon_path = '/home/mohammad/Dossier_travail/Hydrotel/DEH/Atlas_2018/INFO_2018/INFO_TRONCON.mat'
-    workspace = '/home/mohammad/Dossier_travail/Hydrotel/DEH/Atlas_2018/EST_Atlas2018_Subbasin/'
-    Hydroteldir = '/home/mohammad/Dossier_travail/Hydrotel/DEH/Atlas_2018/SETZO_2018/EST_SETZO_2018/physitel'
-    uhrh_to_sub(region, Troncon_path, workspace, Hydroteldir)
+    region_name = 'SLSO_TRONCON'
+    troncon_path = '/home/mohammad/Dossier_travail/Hydrotel/DEH/INFO_TRONCON.mat'
+    workspace = '/home/mohammad/Dossier_travail/Hydrotel/DEH/HRUs_Quebec_meridional/SLSO/20P/'
+    hydroteldir = '/home/mohammad/Dossier_travail/Hydrotel/DEH/MG24HA/SLSO_MG24HA_2020/physitel'
+    uhrh_to_sub(region_name, troncon_path, workspace, hydroteldir)
+    process_lakes(workspace, hydrolakes_file, troncon_info, uhrh_dissolve)
     # main()
--- misc/HRU_to_cellid.py
+++ misc/HRU_to_cellid.py
@@ -19,17 +19,6 @@ points_union = point["geometry"].unary_union
 hru["centroid"] = hru.centroid    
 # Create an union of the other GeoDataFrame's geometries:
 
-# i=0
-# hru['cell_id'] = int(0)
-# for index, row in hru.iterrows():
-#     hru_number = i + 1
-#     hru1 = hru.loc[hru['HRU_ID'] == hru_number] # selects the subbasin a in the subbasin map 
-#     print ('writing HRU :', hru_number )
-#     nearest_geoms = point['geometry'] == nearest_points(hru1['geometry'], points_union)[1]
-#     nearest_data = point.loc[point["geometry"] == nearest_geoms[1]]
-#     nearest_value = point[nearest_data]['station_id'].get_values()[0]
-#     hru.loc[index,'cell_id'] = nearest_value  
-
 
 def nearest(row, geom_union, df1, df2, geom1_col='centroid', geom2_col='geometry', src_column=None):
     """Find the nearest point and return the corresponding value from specified column."""
